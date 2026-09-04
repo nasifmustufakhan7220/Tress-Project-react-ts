eea65512135d467dfe2b5a1c676ea252a2bddff1
Nasif Khan <nasifmustufakhan7220@gmail.com>
2026-09-04 23:00:53 +0600

    navbar done for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import './App.css'
 import type { TreesType } from './Type/type'
 import Trees from './Components/Trees/Trees';
+import Navbar from './Components/Navbar/Navbar';
 
 const treesPromises = async ():Promise<TreesType[]>=>{
     const res = await fetch("https://openapi.programming-hero.com/api/plants");
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <>
+        <Navbar></Navbar>
         <Suspense fallback={<p>Trees are loading.....</p>}>
             <Trees treesPromises={treesPromises()}></Trees>
         </Suspense>
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,19 @@
+
+
 const Navbar = () => {
     return (
         <>
-            <header className="container mx-auto">
+            <header className="container mx-auto px-4 py-6">
+                    <nav className="flex items-center justify-between">
+                        <div className="text-xl">
+                            <i className="fa-solid fa-bars"></i>
+                        </div>
+                        <span className="text-2xl font-bold text-green-700">GreenLeaf</span>
+                        <div className="text-xl">
+                            <i className="fa-solid fa-cart-shopping"></i>
+                        </div>
+                    </nav>
+
                     
             </header>
         </>
